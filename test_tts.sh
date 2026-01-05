6ec5df0235c7be745ffe326e5e2aec9bee2ebeec
Tim-Siu <shuyao@u.nus.edu>
2026-01-05 15:07:12 +0800

    v10 prompt

--- test_tts.sh
+++ test_tts.sh
@@ -4,7 +4,7 @@ set -e
 
 # Configuration
 RESPONSE_JSONL="/Users/tim/Documents/research/trustworthy/output_rollouts/nemotron-nano-30b/aime2025-test-temp_1.0-top_p_1.0-top_k_-1-seed_0-20251231-102840_a100_128k.jsonl"
-OUTPUT_DIR="./tts_test_output_v7"
+OUTPUT_DIR="./tts_test_output_14_24"
 QUESTION_ID="14"
 
 # Set your NVIDIA API key here (or export it before running)
